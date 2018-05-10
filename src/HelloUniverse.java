066264e493355952fda07ed1e29909be4a63e537
yannage <yannissl4@gmail.com>
2018-05-10 19:27:55 -0400

    Added in addition text
    made it a little longer

--- src/HelloUniverse.java
+++ src/HelloUniverse.java
@@ -4,7 +4,7 @@ public class HelloUniverse {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello there Universe!");
-		System.out.println("I can hear you");		
+		System.out.println("I can not hear you from way back here");		
 	}
 
 }
